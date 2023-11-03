35545e022c5d6e51ebe82b7c448f6446a39c77f1
ngocson18 <ngocsonxx98@gmail.com>
2023-11-03 15:15:33 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { environment } from '../environments/environment';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAuKiJ4ojtGz_UyOsCnSJbkZk0nz2W0pic",
+    authDomain: "everything-0311.firebaseapp.com",
+    projectId: "everything-0311",
+    storageBucket: "everything-0311.appspot.com",
+    messagingSenderId: "558217248815",
+    appId: "1:558217248815:web:7d233af11e998a5e99c6da",
+    measurementId: "G-X0B4TLS8JP"
+  }
+};
